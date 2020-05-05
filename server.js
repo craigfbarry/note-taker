77ff7a61b338ac978db5633e37d959daeb673e06
craigfbarry <craigfbarry@gmail.com>
2020-05-05 14:11:58 +0930

    Added routes and the server script. Tested locally on PORT 8080.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+require("./routes/htmlRoutes")(app);
+//require("./routes/apiRoutes")(app);
+
+
+app.listen(PORT, function(){
+    console.log("Listening on PORT : " + PORT);
+})
