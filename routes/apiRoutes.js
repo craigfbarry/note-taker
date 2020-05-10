fa7103f02ea02e9595f04aa341d7d7804f25ee34
Craig Barry <craigfbarry@gmail.com>
2020-05-10 18:32:31 +0930

    Added an index called uniqueID to each object element on posting.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 //const notesData = require("../db/db");
 const fs = require("fs");
-let uniqueID = 1;
+//let uniqueID = 1;
 
 module.exports = function(app){
 
@@ -9,6 +9,7 @@ module.exports = function(app){
     app.get("/api/notes", function(req,res){
         fs.readFile('./db/db.json', (err, data)=>{
             if (err) throw err;
+
             let notesData = JSON.parse(data);
             res.json(notesData);
         });
@@ -16,20 +17,31 @@ module.exports = function(app){
 
 
     app.post("/api/notes", function(req,res){
+        //let uniqueID = 1;
 
 
         fs.readFile('./db/db.json', (err, data)=>{
             if (err) throw err;
+
             let postNotesData = JSON.parse(data);
             postNotesData.push(req.body);
             console.log(postNotesData);
+            //postNotesData.forEach(idFunction);
+
+            for (let i=0;i<postNotesData.length;i++){
+                postNotesData[i].uniqueID = i+1;
+            };
+            console.log(postNotesData);
+
             let postNotesString = JSON.stringify(postNotesData);
             fs.writeFile("./db/db.json", postNotesString, err =>{
                 if (err) throw err;
-                console.log("complete");
+                console.log("Data POSTED");
             });
         });
     });
+
+
 
     app.delete("/api/notes", function(req,res){
         notesData.push(req.body);
